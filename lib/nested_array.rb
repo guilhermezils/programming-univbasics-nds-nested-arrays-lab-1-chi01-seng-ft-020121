70d23cced1aa5a2d45903b423ac79f75c340d1f2
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-04 03:11:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,7 +42,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
  
-binding.pry
+
 assembled_matrix[1][1]
 
 
