40eb3a2e527f764810d979289dff4349af351dea
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-04 03:10:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,5 +1,5 @@
 # Examples inspired by U. S. National Organic Standards
-
+require 'pry'
 ORGANIC_PRODUCE = [
   "Strawberries",
   "Potatoes",
@@ -42,7 +42,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
  
-
+binding.pry
 assembled_matrix[1][1]
 
 
